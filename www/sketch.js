49c158a40234b41c9c643d7947d4e1c4241c2c2b
kchaeys2 <kchaeys2@naver.com>
2022-12-02 16:42:33 +0900

    iphone

--- www/sketch.js
+++ www/sketch.js
@@ -1,20 +1,22 @@
 var wave;
 var playing = false;
 var button;
+var ampValue = 0;
 
 function setup() {
   createCanvas(640, 480);
 
   wave = new p5.Oscillator();
   wave.setType('sine');
-  wave.freq(440);
-  wave.amp(0.1);
+  //wave.freq(440);
+  wave.amp(0);
 
   button = createButton('play/pause');
   button.mousePressed(toggle);
 }
 
 function draw() {
+  wave.freq(ampValue);
 }
 
 function toggle(){
@@ -26,4 +28,9 @@ function toggle(){
     wave.amp(0,1);
     playing = false;
   }
+}
+function deviceMoved(){
+  ampValue = accelerationX*1000;
+  background(255,255,255);
+  Text(accelerationX,10,10);
 }
