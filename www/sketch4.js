d10480e02dc8ca6c8b722c1afeb97c2c1ab8a69d
kchaeys2 <kchaeys2@naver.com>
2022-12-10 14:42:11 +0900

    realfin

--- www/sketch4.js
+++ www/sketch4.js
@@ -61,7 +61,7 @@ function draw(){
   r = floor(map(rotationX, 0, 180, 0,10));
   text(r,windowWidth/2,windowHeight+10);
   if(r < 0){
-    r = 0;
+    r = -1;
   }
   notes = [0+(12*r),1+(12*r),2+(12*r),3+(12*r),4+(12*r),5+(12*r),
            6+(12*r),7+(12*r),8+(12*r),9+(12*r),10+(12*r),11+(12*r)];
